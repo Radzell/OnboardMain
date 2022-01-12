baa6c4e740817cf0cceb52ff54b4fc4623c96653
Deon Robinson <deon@appmunki.com>
2022-01-11 22:25:41 -0800

    WIP

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,46 @@
 import * as functions from "firebase-functions";
+import * as admin from 'firebase-admin';
+import * as cors from 'cors'
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
+admin.initializeApp()
+
+const corsHandler = cors({origin: true});
+
+interface Flow {
+    name?: string
+    color?: string
+    tagLine?: string
+    elements: any[]
+
+}
+
 export const helloWorld = functions.https.onRequest((request, response) => {
     functions.logger.info("Hello logs!", { structuredData: true });
     response.send("Hello from Firebase!");
+});
+
+export const getFlow = functions.https.onRequest(async (req, res) => {
+    return corsHandler(req, res, async () => {
+		const flowId = req.query.flowId as string
+
+        console.log('flowId', flowId)
+        if(!flowId) {
+            res.status(400).send("invalid flowId")
+            return
+        }
+
+        const flowSnap = await admin.firestore().collection(`/flows`).doc(flowId).get()
+
+        if(!flowSnap.exists) {
+            res.status(400).send("invalid flowId")
+            return
+        }
+
+        const flow = flowSnap.data() as Flow
+
+        functions.logger.info("Hello flow", flow);
+
+
+        res.send(flow);
+    })
 });
--- lib/src/index.tsx
+++ lib/src/index.tsx
@@ -1,12 +1,74 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from 'axios';
+import localforage from 'localforage'
+import { ThemeProvider } from 'emotion-theming'
+import theme from '@rebass/preset'
 
-export const OnboardOS = ({flowId} : {flowId: string}): JSX.Element  => {
-    useEffect(() => {
-        axios.get("")
-    },[])
-    
+interface Flow {
+    name?: string
+    color?: string
+    tagLine?: string
+    elements: any[]
+
+}
+
+function isObject(object:any) {
+    return object != null && typeof object === 'object';
+  }
+
+function deepEqual(object1:any, object2:any) {
+    const keys1 = Object.keys(object1);
+    const keys2 = Object.keys(object2);
+    if (keys1.length !== keys2.length) {
+      return false;
+    }
+    for (const key of keys1) {
+      const val1 = object1[key];
+      const val2 = object2[key];
+      const areObjects = isObject(val1) && isObject(val2);
+      if (
+        areObjects && !deepEqual(val1, val2) ||
+        !areObjects && val1 !== val2
+      ) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+export const OnboardOS = ({ flowId }: { flowId: string }): JSX.Element => {
+    const [flow, setFlow] = useState <Flow | null>(null)
+
+        useEffect(() => {
+            const getFlow = async () => {
+                const result = await axios.get(`https://us-central1-onboard-os.cloudfunctions.net/getFlow?flowId=${flowId}`)
+
+                const flow = result.data as Flow
+                console.log('flow', flow)
+
+                await localforage.setItem(flowId, flow)
+                setFlow(flow)
+            }
+
+            getFlow()
+        }, [flowId])
+
+        useEffect(() => {
+            const getFlow = async () => {
+                const _flow = await localforage.getItem(flowId) as Flow | null
+
+                if(_flow && deepEqual(_flow, flow)) {
+                    setFlow(_flow)
+                }
+            }
+
+            getFlow()
+        }, [])
+
     return (
-        <div>flowid</div>
+        <ThemeProvider theme={theme}>
+            {!flow && <div>Loading</div>}
+
+        </ThemeProvider>
     )
 }
